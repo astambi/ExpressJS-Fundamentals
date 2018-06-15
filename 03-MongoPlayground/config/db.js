5893e9f0658626c15d908ac5e3b9d22781d096eb
Anna Stambolieva <annsta@gmail.com>
2018-06-16 02:05:32 +0300

    del

--- 03-MongoPlayground/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.Promise = global.Promise; // replacing default mongoose promise
-
-require("../models/ImageSchema");
-require("../models/TagSchema");
-
-const connectionString = "mongodb://localhost:27017/mongoplayground";
-
-module.exports = mongoose.connect(connectionString); // export promise
--- 08-Exam Prep - Doner Place - Copy/server/controllers/home-controller.js
+++
@@ -1,9 +0,0 @@
-const productService = require("../services/product-service");
-
-module.exports = {
-    index: async (req, res) => {
-        const products = await productService.getAll(); // grouped by category
-        
-        return res.render("home/index", { products });
-    }
-};
